0f7de7100e31450f0d82f852f4649090d2cb40e1
hexiufeng <hexiufeng@reta-inc.com>
2022-05-09 15:26:16 +0800

    fmt code

--- src/main/java/com/jkqj/magick/log/MagickLog.java
+++ src/main/java/com/jkqj/magick/log/MagickLog.java
@@ -7,7 +7,7 @@ public class MagickLog {
     private static final Logger LOGGER = LoggerFactory.getLogger(MagickLog.class);
 
     public static void logMagickException(int errorNumber, String reason, String description, String from) {
-        LOGGER.info("magick error: errorNumber:{}, reason:{}, desc:{}, from:{}", errorNumber, reason, description, from);
+        LOGGER.info("magick error: errorNumber:{}, reason:{}, desc:{}, from: {}", errorNumber, reason, description, from);
     }
 
     public static void logMagickInfo(String message,String from){
