d3764cd01487357aed44f7c9f2599ffc012d26f9
hexiufeng <hexiufeng@reta-inc.com>
2022-05-13 10:48:36 +0800

    add quick scale and make install

--- jni/build.sh
+++ jni/build.sh
@@ -12,8 +12,8 @@ make
 
 if [ $OS_NAME = "Darwin" ];then
   # Mac OS X 操作系统
-  echo "please run: sudo cp build/libMagick4J.dylib /usr/local/lib"
-  sudo cp libMagick4J.dylib /usr/local/lib/
+  echo "mac os, and will install"
+  sudo make install
 else
   # GNU/Linux操作系统
   echo "linux, and will install"
--- src/main/java/com/jkqj/magick/image/ImageTransform.java
+++ src/main/java/com/jkqj/magick/image/ImageTransform.java
@@ -1,25 +1,24 @@
 package com.jkqj.magick.image;
 
 import com.jkqj.magick.BaseMagick;
+import com.jkqj.magick.support.GifSupport;
 
 /**
  * 图片转换工具
  *
  * @author hexiufeng
- *
  */
 public class ImageTransform extends BaseMagick {
     /**
-     *
      * @param imageHandler
      * @param orientation  {@link OrientationType}
      * @return
      */
-    private static native  long autoOrientImage(long imageHandler, int orientation);
+    private static native long autoOrientImage(long imageHandler, int orientation);
 
-    private static native long chopImage(long imageHandler,long x,long y, int width, int height);
+    private static native long chopImage(long imageHandler, long x, long y, int width, int height);
 
-    private static native long cropImage(long imageHandler,long x,long y, int width, int height);
+    private static native long cropImage(long imageHandler, long x, long y, int width, int height);
 
     private static native long flipImage(long imageHandler);
 
@@ -33,13 +32,13 @@ public class ImageTransform extends BaseMagick {
     private static native long trimImage(long imageHandler);
 
 
-    public static Image autoOrientImage(Image src, int orientation){
+    public static Image autoOrientImage(Image src, int orientation) {
         long handler = autoOrientImage(src.getHandler(), orientation);
         return Image.factory(handler);
     }
 
-    public static Image chopImage(Image src,long x,long y, int width, int height){
-        long handler = chopImage(src.getHandler(),x,y,width,height);
+    public static Image chopImage(Image src, long x, long y, int width, int height) {
+        long handler = chopImage(src.getHandler(), x, y, width, height);
         return Image.factory(handler);
     }
 
@@ -54,33 +53,68 @@ public class ImageTransform extends BaseMagick {
      * @param height
      * @return
      */
-    public static Image cropImage(Image src,long x,long y, int width, int height){
-        long handler = cropImage(src.getHandler(),x,y,width,height);
+    public static Image cropImage(Image src, long x, long y, int width, int height) {
+        long handler = cropImage(src.getHandler(), x, y, width, height);
         return Image.factory(handler);
     }
 
-    public static  Image flipImage(Image src){
+    public static Image flipImage(Image src) {
         long handler = flipImage(src.getHandler());
         return Image.factory(handler);
     }
 
-    public static  Image flopImage(Image src){
+    public static Image flopImage(Image src) {
         long handler = flopImage(src.getHandler());
         return Image.factory(handler);
     }
 
-    public static  Image transposeImage(Image src){
+    public static Image transposeImage(Image src) {
         long handler = transposeImage(src.getHandler());
         return Image.factory(handler);
     }
 
-    public static  Image transverseImage(Image src){
+    public static Image transverseImage(Image src) {
         long handler = transverseImage(src.getHandler());
         return Image.factory(handler);
     }
 
-    public static  Image trimImage(Image src){
+    public static Image trimImage(Image src) {
         long handler = trimImage(src.getHandler());
         return Image.factory(handler);
+    }
+
+    public static boolean quickScaleImage(String srcImgFile, String dstImgFile, int columns, int rows, boolean useThumbnail) {
+        Image src = Image.factory(srcImgFile, true);
+        if (src == null) {
+            return false;
+        }
+
+        String fmt = src.getFormat();
+        src.destroy();
+        if (fmt.equalsIgnoreCase("GIF")) {
+            if (useThumbnail) {
+                return GifSupport.thumbnail(srcImgFile, dstImgFile, columns, rows);
+            }
+            return GifSupport.scale(srcImgFile, dstImgFile, columns, rows);
+        }
+
+        src = Image.factory(srcImgFile);
+
+        Image dstImage = null;
+
+        if (useThumbnail) {
+            dstImage = src.thumbnailImage(columns, rows);
+        } else {
+            dstImage = src.scaleImage(columns, rows);
+        }
+        src.destroy();
+        if (dstImage == null) {
+            return false;
+        }
+        boolean ret = dstImage.writeImage(dstImgFile);
+
+        dstImage.destroy();
+
+        return ret;
     }
 }
